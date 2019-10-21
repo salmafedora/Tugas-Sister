2824770e39f31ac7d786260b81ecc97e62949e06
salmafedora <48651019+salmafedora@users.noreply.github.com>
2019-10-21 23:37:11 +0700

    Add files via upload

---
+++ Tubes/signup.py
@@ -0,0 +1,58 @@
+# 3rd party modules
+from flask import make_response, abort
+
+# Data to serve with our API
+
+
+USERPRIVATE = {
+    "leo": {
+        "username": "leo",
+        "email": "leo.draconus@gmail.com",
+        "password": "qwerty",
+        "publicdata": {
+            "nama": "Leo Draconus",
+            "bidang": "musik",
+            "prestasi": "juara 2 lomba gitar akustik"
+        }
+    },
+    "catto": {
+        "username": "catto",
+        "email": "catto.kawaii@gmail.com",
+        "password": "abcde",
+        "publicdata": {
+            "nama": "Catto Kawaii",
+            "bidang": "cosplayer",
+            "prestasi": "juara 3 lomba cosplay"
+        }
+    }
+}
+
+def create(username):
+
+    username = person.get("username", None)
+    email = person.get("email", None)
+    password = person.get("password", None)
+    nama = person.get("nama", None)
+    bidang = person.get("bidang", None)
+    prestasi = person.get("prestasi", None)
+
+    # Does the person exist already?
+    if username not in USER and username is not None:
+        USERPRIVATE[username] = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "nama": nama,
+            "bidang": bidang,
+            "prestasi": prestasi,
+        }
+        return make_response(
+            "{username} successfully created".format(username=username), 201
+        )
+
+    # Otherwise, they exist, that's an error
+    else:
+        abort(
+            406,
+            "Person with last name {username} already exists".format(username=username),
+        )
