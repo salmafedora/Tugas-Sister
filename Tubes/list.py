c74330121c165060a861c1f77dcddad664ddd3d6
salmafedora <48651019+salmafedora@users.noreply.github.com>
2019-10-21 23:28:46 +0700

    Update list.py

--- Tubes/list.py
+++ Tubes/list.py
@@ -1,31 +1,24 @@
+# 3rd party modules
+from flask import make_response, abort
+
 # Data to serve with our API
 
 
 USER = {
     "leo": {
-        "username": "leo",
-        "email": "leo.draconus@gmail.com",
-        "password": "qwerty",
-        "publicdata": {
-            "nama": "Leo Draconus",
-            "bidang": "musik",
-            "prestasi": "juara 2 lomba gitar akustik"
-        }
+        "nama": "Leo Draconus",
+        "bidang": "musik",
+        "prestasi": "juara 2 lomba gitar akustik"
     },
     "catto": {
-        "username": "catto",
-        "email": "catto.kawaii@gmail.com",
-        "password": "abcde",
-        "publicdata": {
-            "nama": "Catto Kawaii",
-            "bidang": "cosplayer",
-            "prestasi": "juara 3 lomba cosplay"
-        }
+        "nama": "Catto Kawaii",
+        "bidang": "cosplayer",
+        "prestasi": "juara 3 lomba cosplay"
     }
 }
 
 # Create a handler for our read (GET) user
-def read():
+def read_all():
     """
     This function responds to a request for /api/user
     with the complete lists of user
@@ -34,3 +27,17 @@ def read():
     """
     # Create the list of people from our data
     return [USER[key] for key in sorted(USER.keys())]
+
+def read_one(username):
+
+    # Does the person exist in people?
+    if username in USER:
+        mahasiswa = USER.get(username)
+
+    # otherwise, nope, not found
+    else:
+        abort(
+            404, "Person with username {username} not found".format(username=username),
+        )
+
+    return mahasiswa
