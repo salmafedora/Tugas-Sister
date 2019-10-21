3a1a5ae74dc89144dba8fb53e850a9cf594d13d5
salmafedora <48651019+salmafedora@users.noreply.github.com>
2019-10-21 23:45:41 +0700

    Add files via upload

---
+++ Tubes/login.py
@@ -0,0 +1,38 @@
+# 3rd party modules
+from flask import make_response, abort
+
+# Data to serve with our API
+
+
+USER_PRIVATE = {
+    "leo": {
+        "username": "leo",
+        "password": "qwerty"
+    },
+    "catto": {
+        "username": "catto",
+        "password": "abcde"
+    }
+}
+
+def enter(username):
+
+    username = person.get("username", None)
+    password = person.get("password", None)
+
+    # Does the person exist already?
+    if username not in USER_PRIVATE and username is not None:
+        USER_PRIVATE[username] = {
+            "username": username,
+            "password": password,
+        }
+        return make_response(
+            "{username} successfully logged in".format(username=username), 201
+        )
+
+    # Otherwise, they exist, that's an error
+    else:
+        abort(
+            406,
+            "Person with last name {username} already logged in".format(username=username),
+        )
