391960fed258ef9ccfe7d2c46adc929c5661f09e
salmafedora <48651019+salmafedora@users.noreply.github.com>
2019-10-21 20:58:42 +0700

    Add files via upload

---
+++ Tubes/userlistusername.py
@@ -0,0 +1,36 @@
+# Data to serve with our API
+
+
+USERLISTUSERNAME = {
+    "leo": {
+        "username": "leo",
+        "email": "leo.draconus@gmail.com",
+        "password": "qwerty",
+        "publicdata": {
+            "nama": "Leo Draconus",
+            "bidang": "musik",
+            "prestasi": "juara 2 lomba gitar akustik"
+        }
+    },
+    "catto": {
+        "username": "catto",
+        "email": "catto.kawaii@gmail.com",
+        "password": "abcde",
+        "publicdata": {
+            "nama": "Catto Kawaii",
+            "bidang": "cosplayer",
+            "prestasi": "juara 3 lomba cosplay"
+        }
+    }
+}
+
+# Create a handler for our read (GET) user
+def read():
+    """
+    This function responds to a request for /api/people
+    with the complete lists of user
+
+    :return:        sorted list of user
+    """
+    # Create the list of people from our data
+    return [USERLISTUSERNAME[key] for key in sorted(USERLISTUSERNAME.keys())]
